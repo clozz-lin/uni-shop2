d87b2223fc239b9e0ee0f0d4761fb0c8f1635b7b
夕小汐lin <885204655@qq.com>
2022-07-27 23:51:30 +0800

    完成了分类页面的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,19 +1,120 @@
 <template>
   <view>
-    
+    <view class="scroll-view-container">
+      <scroll-view scroll-y="true" class="left-scroll-view" :style="{ height: wh + 'px' }">
+        <block v-for="(item, i) in cateList" :key="i">
+          <view :class="['left-scroll-view-item', i === active ? 'active' : '']" @click="activeChange(i)">{{ item.cat_name }}</view>
+        </block>
+      </scroll-view>
+      <scroll-view scroll-y="true" class="right-scroll-view" :style="{ height: wh + 'px' }" :scroll-top="scrollTop">
+        <view class="cate-lv2" v-for="(item2, i2) in cateLevel2" :key="i2">
+          <view class="cate-lv2-title">/{{ item2.cat_name }}/</view>
+          <!-- 三级分类 -->
+          <view class="cate-lv3-list">
+            <view class="cate-lv3-item" v-for="(item3, i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
+              <image :src="item3.cat_icon"></image>
+              <text>{{ item3.cat_name }}</text>
+            </view>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        
-      };
+export default {
+  data() {
+    return {
+      wh: 0,
+      cateList: [],
+      // 二级分类列表
+      cateLevel2: [],
+      active: 0,
+      // 滚动条重置
+      scrollTop: 0
+    };
+  },
+  onLoad() {
+    const sysInfo = uni.getSystemInfoSync();
+    this.wh = sysInfo.windowHeight;
+    this.getCateList();
+  },
+  methods: {
+    async getCateList() {
+      const { data: res } = await uni.$http.get('/api/public/v1/categories');
+      if (res.meta.status !== 200) return uni.$showMsg();
+      this.cateList = res.message;
+      // 为二级分类赋值
+      this.cateLevel2 = res.message[0].children;
+    },
+    activeChange(i) {
+      this.active = i;
+      // 为二级分类列表重新赋值
+      this.cateLevel2 = this.cateList[i].children;
+      this.scrollTop = this.scrollTop === 0 ? 1 : 0;
+    },
+    //
+    gotoGoodsList(item3) {
+      uni.navigateTo({
+        url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+      });
     }
   }
+};
 </script>
 
 <style lang="scss">
-
+.scroll-view-container {
+  display: flex;
+  .left-scroll-view {
+    width: 120px;
+    .left-scroll-view-item {
+      line-height: 60px;
+      background-color: #f7f7f7;
+      text-align: center;
+      font-size: 12px;
+      &.active {
+        background-color: #ffffff;
+        position: relative;
+        // 渲染激活项左侧的红色指示边线
+        &::before {
+          content: ' ';
+          display: block;
+          width: 3px;
+          height: 30px;
+          background-color: #c00000;
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      }
+    }
+  }
+}
+.cate-lv2-title {
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  padding: 15px 0;
+}
+.cate-lv3-list {
+  display: flex;
+  flex-wrap: wrap;
+  .cate-lv3-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 33.33%;
+    margin-bottom: 10px;
+    image {
+      width: 60px;
+      height: 60px;
+    }
+    text {
+      font-size: 12px;
+    }
+  }
+}
 </style>
